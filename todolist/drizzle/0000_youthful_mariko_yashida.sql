3024d7dc2d1e138473b79b5b4ba2b01cd90e5e28
bloop <diogofalardo04@gmail.com>
2026-04-15 10:59:00 +0100

    initial commit

--- todolist/drizzle/0000_youthful_mariko_yashida.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `todos` (
-	`id` char(36) NOT NULL DEFAULT (uuid()),
-	`name` text NOT NULL,
-	`is_complete` boolean NOT NULL,
-	`created_at` timestamp NOT NULL DEFAULT (now()),
-	`updated_at` timestamp NOT NULL DEFAULT (now()) ON UPDATE CURRENT_TIMESTAMP,
-	CONSTRAINT `todos_id` PRIMARY KEY(`id`)
-);
